4a42a7ea2bd8b96e5f169f3931305592cb9d778c
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2025-08-09 05:35:54 +0600

    edit store

---
+++ edit_category.php
@@ -0,0 +1,61 @@
+<?php
+require("connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Category</title>
+</head>
+
+<body>
+    <?php
+
+    if (isset($_GET["id"])) {
+        $getid = $_GET["id"];
+
+        $sql = "SELECT * FROM category WHERE category_id=$getid";
+
+        $query = $conn->query($sql);
+        $data = mysqli_fetch_assoc($query);
+        $category_id = $data["category_id"];
+        $category_name = $data["category_name"];
+        $category_entrydate = $data["category_entrydate"];
+
+
+
+    }
+    ;
+
+    if (isset($_GET["category_name"])) {
+
+        $new_category_id = $_GET["category_id"];
+        $new_category_name = $_GET["category_name"];
+        $new_category_entrydate = $_GET["category_entrydate"];
+
+        $sql1 = "UPDATE category SET category_name='$new_category_name', category_entrydate='$new_category_entrydate' WHERE category_id='$new_category_id'";
+
+        if ($conn->query($sql1)) {
+            echo "Update Successful";
+        } else {
+            echo "Not Update";
+        }
+    }
+
+    ?>
+    <form action="edit_category.php" method="GET">
+        Category: <br>
+        <input type="text" name="category_name" value="<?php echo $category_name ?>"><br> <br>
+        Category Entry Date: <br>
+        <input type="date" name="category_entrydate" value="<?php echo $category_entrydate ?>"><br><br>
+        <input type="text" name="category_id" value="<?php echo $category_id ?>" hidden>
+        <input type="submit" value="update">
+
+    </form>
+
+</body>
+
+</html>
